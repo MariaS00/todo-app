c847fb4d99fcdf257c79afc7df501377edc70587
MariaS00 <maria.szyszka@artifact.com>
2022-10-12 16:15:17 +0100

    Initial commit

---
+++ src/main/java/io/github/mmm/model/Task.java
@@ -0,0 +1,43 @@
+package io.github.mmm.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Table(name ="tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @NotBlank(message = "Taks's description cannot be empty")
+    private String description;
+    private boolean done;
+
+    public Task() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+}
---
+++ src/main/java/io/github/mmm/model/TaskRepository.java
@@ -0,0 +1,11 @@
+package io.github.mmm.model;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+interface TaskRepository extends JpaRepository<Task, Integer> {
+
+
+}
