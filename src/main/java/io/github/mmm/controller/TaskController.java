1ef938479557ba06bcfcbf72a14511d98d348e52
MariaS00 <maria.szyszka@artifact.com>
2022-10-13 16:04:47 +0100

    after 40

--- src/main/java/io/github/mmm/controller/TaskController.java
+++ src/main/java/io/github/mmm/controller/TaskController.java
@@ -5,10 +5,12 @@ import io.github.mmm.model.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.data.domain.Pageable;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,5 +35,25 @@ public class TaskController {
         return ResponseEntity.ok(repository.findAll(page).getContent());
     }
 
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateTask(@PathVariable int id, @RequestBody @Valid Task toUpdate){
+        if (!repository.existsById(id)){
+            return ResponseEntity.notFound().build();
+        }
+        toUpdate.setId(id);
+        repository.save(toUpdate);
+        return ResponseEntity.noContent().build();
+    }
 
+    @GetMapping("/tasks/{id}")
+    ResponseEntity<Task> readTask(@PathVariable int id){
+        return repository.findById(id).map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/tasks")
+    ResponseEntity<Task> createTask(@RequestBody @Valid Task toSave){
+        Task result = repository.save(toSave);
+        return ResponseEntity.created(URI.create("/" + result.getId())).body(result);
+    }
 }
